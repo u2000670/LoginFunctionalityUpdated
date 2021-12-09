f8836c543ef37a022164480da3720a2c35152f0b
Faris-Faiz <u2000421@siswa.um.edu.my>
2021-12-09 11:32:04 +0800

    First Commit

---
+++ src/updatedloginfunctionality/ConnectDatabase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package updatedloginfunctionality;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Faris Faiz
+ */
+public class ConnectDatabase {
+    public static Connection connectdb() {
+        Connection con = null;
+        String url = "jdbc:derby://localhost:1527/userlogin";
+        String username = "app";
+        String password = "app";
+        try {
+            con = DriverManager.getConnection(url, username, password);
+            return con;
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
---
+++ src/updatedloginfunctionality/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package updatedloginfunctionality;
+
+import javax.swing.*;
+import java.sql.*;
+
+/**
+ *
+ * @author Faris Faiz
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new LoginForm1().setVisible(true);
+    }
+}
